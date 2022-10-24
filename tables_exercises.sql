fdbf2af4c05e905f52813e3baca2d297b6e4f7a8
JoshuaHolt-Tech <116033033+JoshuaHolt-Tech@users.noreply.github.com>
2022-10-24 13:44:16 -0500

    adding sql tables exercise.

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -1,17 +1,21 @@
+
+-- Use the employees database. Write the SQL code necessary to do this.
 USE employees;
 
+-- List all the tables in the database. Write the SQL code necessary to accomplish this.
 SHOW TABLES;
 
+-- Explore the employees table. What different data types are present on this table?
 DESCRIBE employees;
 
 -- Question 6: "Which table(s) do you think contain a numeric type column?"
--- Answer: emp_no - int.
+-- Answer: I think all tables will contain a numeric tpye column.
 
 -- Question 7: "Which table(s) do you think contain a string type column?"
--- Answer: first_name, last_name and gender tables.
+-- Answer: I think all tables except salaries will contain a string type column.
 
 -- Question 8: "Which table(s) do you think contain a date type column?"
--- Answer: birth_date and hire_date tables.
+-- Answer: dept_manager, employees, salaries, and titles will have data type collumn.
 
 -- Question 9: "What is the relationship between the employees and the departments tables?"
 -- Answer: The emp_no is linked to the departments table through the dept_emp table. Specifically the emp_no and the dept_no fields.
